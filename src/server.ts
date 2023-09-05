3e3e18ac9f72c483acc835066c233f1664740200
Gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-09-05 09:31:24 -0300

    docs: install cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { router } from "./routes/router";
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(cors);
 
 app.listen(3001, () => console.log("Server is Running on port 3001"));
